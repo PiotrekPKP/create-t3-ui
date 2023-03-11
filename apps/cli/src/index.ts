0cac90d2f8baa72396208df010634355e47ce42c
PiotrekPKP <peterpkppeter@gmail.com>
2023-03-11 07:58:13 +0100

    feat: add UI starting

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,4 +1,27 @@
 #! /usr/bin/env node
-console.log("Hello World");
+import { execa } from "execa";
+import ora from "ora";
+
+const CWD = `${process.cwd()}/apps/web`;
+
+try {
+  const command = execa("npm", ["start"], { cwd: CWD });
+
+  const commandSpinner = ora("Starting the UI...").start();
+
+  await new Promise<void>((res, rej) => {
+    command.stdout?.on("data", (data: Buffer) => {
+      const str = data.toString();
+
+      if (str.includes("ready - started")) {
+        commandSpinner.succeed("UI started!");
+      }
+    });
+    command.on("error", (e) => rej(e));
+    command.on("close", () => res());
+  });
+} catch (e) {
+  console.log("Could not run the UI...", e);
+}
 
 export {};
