1e3c31445aa3b3d2738812dda03fd46f56a46837
PiotrekPKP <peterpkppeter@gmail.com>
2023-03-11 20:53:00 +0100

    feat: add button to the done page

--- src/pages/done.tsx
+++ src/pages/done.tsx
@@ -1,5 +1,8 @@
 import { type NextPage } from "next";
+import Link from "next/link";
 import { useEffect } from "react";
+import { Button } from "~/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 const Done: NextPage = () => {
   useEffect(() => {
@@ -12,6 +15,13 @@ const Done: NextPage = () => {
     <div>
       <h1>Your project is ready!</h1>
       <p>Thanks for using Create T3 App!</p>
+
+      <div className="mt-12">
+        <Link href="/" className="flex w-fit items-center gap-2">
+          <ArrowRight width={18} height={18} />
+          <Button variant="link">Create another project</Button>
+        </Link>
+      </div>
     </div>
   );
 };
